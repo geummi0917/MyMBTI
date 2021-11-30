76ad3fc7232bf5560848056b8d6d3ecd297f7c48
geummi <geummi0917@gmail.com>
2021-12-01 00:54:55 +0900

    메인페이지 css 수정

--- js/start.js
+++ js/start.js
@@ -4,6 +4,35 @@
     const main = document.querySelector('.section--main');
     const qna = document.querySelector('.section--qna');
 
+    function addAnswer(answerText, qIdx) {
+        var a = document.querySelector('.qna__answerbox');
+        var answer = document.createElement('button');
+        answer.classList.add('answerList');
+        a.appendChild(answer);
+        answer.innerHTML = answerText;
+
+        answer.addEventListener("click", function() {
+            var children = document.querySelectorAll('.answerList');
+            for(let i=0; i<children.length; i++) {
+                children[i].disabled = true;
+                children[i].style.display = "none";
+            }
+            goNext(++qIdx);
+        }, false);
+    }
+
+    function goNext(qIdx) {
+        var q = document.querySelector('.qna__qbox');
+        var qText = document.createElement('h3');
+        q.appendChild(qText);
+        qText.innerHTML = qnaList[qIdx].q;
+        // q.innerHTML = qnaList[qIdx].q;
+
+        for(let i in qnaList[qIdx].a) {
+            addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+        }
+    }
+
     $('.btn-start').click(function() {
         main.style.WebkitAnimation = "fadeOut 1s";
         main.style.Animation = "fadeOut 1s";
@@ -14,6 +43,8 @@
                 main.style.display = "none";
                 qna.style.display = "block";
             }, 500)
+            let qIdx = 0;
+            goNext(qIdx);
         }, 500)
         
     })
