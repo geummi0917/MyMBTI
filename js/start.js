dc17b277df5f2aaa503be955b0cb64840e05d660
geummi <geummi0917@gmail.com>
2021-11-30 22:43:01 +0900

    메인페이지 화면전환효과 추가

--- js/start.js
+++ js/start.js
@@ -4,15 +4,18 @@
     const main = document.querySelector('.section--main');
     const qna = document.querySelector('.section--qna');
 
-    // function start() {
-    //     console.log("클릭");
-    //     main.style.display = "none";
-    //     qna.style.display = "block";
-    // }
-
     $('.btn-start').click(function() {
-        main.style.display = "none";
-        qna.style.display = "block";
+        main.style.WebkitAnimation = "fadeOut 1s";
+        main.style.Animation = "fadeOut 1s";
+        setTimeout(() => {
+            qna.style.WebkitAnimation = "fadeIn 1s";
+            qna.style.Animation = "fadeIn 1s";
+            setTimeout(() => {
+                main.style.display = "none";
+                qna.style.display = "block";
+            }, 500)
+        }, 500)
+        
     })
 
 })(window, document)
