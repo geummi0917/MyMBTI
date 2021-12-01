b0bfd3d011c04e58e50f782096cdce51c6410cf0
geummi <geummi0917@gmail.com>
2021-12-02 01:23:20 +0900

    카카오톡 공유까지

---
+++ js/share.js
@@ -0,0 +1,33 @@
+const url = 'https://geummi0917.github.io/MyMBTI/';
+
+function setShare() {
+    var resultImg = document.querySelector('.result__img');
+    var resultAlt = resultImg.firstElementChild.alt;
+
+    const shareTitle = "십이간지 연애유형 결과";
+    const shareDesc = infoList[resultAlt].name;
+    const shareImg = url + 'img/image-' + resultAlt + '.png';
+    const shareURL = url + 'page.result-' + resultAlt + '.html';
+
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: shareTitle,
+            description: shareDesc,
+            imageUrl: shareImg,
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL
+            },
+        },
+        buttons: [{
+                title: '결과 확인하기',
+                link: {
+                    mobileWebUrl: shareURL,
+                    webUrl: shareURL
+                },
+            },
+        ]
+    });
+
+}
