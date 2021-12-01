835559db7e7e41e8553ab5b5b4b5683b739793f2
geummi <geummi0917@gmail.com>
2021-12-02 01:45:44 +0900

    카톡 공유화면 오류 수정

--- js/share.js
+++ js/share.js
@@ -7,7 +7,7 @@ function setShare() {
     const shareTitle = "십이간지 연애유형 결과";
     const shareDesc = infoList[resultAlt].name;
     const shareImg = url + 'img/image-' + resultAlt + '.png';
-    const shareURL = url + 'page.result-' + resultAlt + '.html';
+    const shareURL = url + 'page/result-' + resultAlt + '.html';
 
     Kakao.Link.sendDefault({
         objectType: 'feed',
